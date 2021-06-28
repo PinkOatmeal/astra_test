9cbf2141050d27da4881b8c8e7b94ac67b7aea8b
Ilya Zhalnin <mr.ilay333@gmail.com>
2021-06-29 00:07:53 +0300

    Добавляет базовый набор схем

---
+++ src/schemas/Geo.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class BaseGeoSchema(BaseModel):
+    ip: str
+
+
+class GeoSchemaCreate(BaseModel):
+    pass
+
+
+class GeoSchemaCountry(BaseGeoSchema):
+    country: str
+    country_iso: str
+
+    class Config:
+        orm_mode = True
+
+
+class GeoSchemaCity(BaseGeoSchema):
+    country: str
+    country_iso: str
+    city: str
+
+    class Config:
+        orm_mode = True
+
+
+class GeoSchemaFull(BaseGeoSchema):
+    ip: str
+    continent: str
+    continent_code: str
+    country: str
+    country_iso: str
+    city: Optional[str]
+    latitude: float
+    longitude: float
+    time_zone: str
+    postal: Optional[str]
+    metro_code: Optional[int]
+
+    class Config:
+        orm_mode = True
