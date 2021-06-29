ac55ca7d442b34e81e1c1859f108f400d507da57
Ilya Zhalnin <mr.ilay333@gmail.com>
2021-06-29 17:57:33 +0300

    Добавляет методы для создания записей из схем

--- src/db/crud/geo/geo_crud.py
+++ src/db/crud/geo/geo_crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from src.db.models.Geo import Geo
-from src.schemas.Geo import GeoSchemaFull
+from src.schemas.Geo import GeoSchemaFull, GeoSchemaFromCountryJson
 
 
 class GeoCrud:
@@ -9,7 +9,7 @@ class GeoCrud:
     def __init__(self, db: Session) -> None:
         self.db = db
 
-    def create(self, geo: GeoSchemaFull) -> Geo:
+    def create_with_full_info(self, geo: GeoSchemaFull) -> Geo:
         geo_to_db: Geo = Geo(
             ip=geo.ip,
             continent=geo.continent,
@@ -22,6 +22,19 @@ class GeoCrud:
             time_zone=geo.time_zone,
             postal=geo.postal,
             metro_code=geo.metro_code)
+        self.db.add(geo_to_db)
+        self.db.commit()
+        self.db.refresh(geo_to_db)
+        return geo_to_db
+
+    def create_with_country(self, geo: GeoSchemaFromCountryJson) -> Geo:
+        geo_to_db: Geo = Geo(
+            ip=geo.ip,
+            continent=geo.continent,
+            continent_code=geo.continent_code,
+            country=geo.country,
+            country_iso=geo.country_iso)
+
         self.db.add(geo_to_db)
         self.db.commit()
         self.db.refresh(geo_to_db)
