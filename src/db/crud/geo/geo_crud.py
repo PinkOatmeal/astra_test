f8fb6e947ecb05a4f4e3c426d4e28461759c6e37
Ilya Zhalnin <mr.ilay333@gmail.com>
2021-06-29 20:06:24 +0300

    Добавляет методы для обновления записей в базе

--- src/db/crud/geo/geo_crud.py
+++ src/db/crud/geo/geo_crud.py
@@ -46,5 +46,39 @@ class GeoCrud:
     def read_by_id(self, _id: int) -> Optional[Geo]:
         return self.db.query(Geo).filter(Geo.id == _id).first()
 
+    def update_with_full_info(self, geo: GeoSchemaFull) -> Geo:
+        geo_from_db: Geo = self.db.query(Geo).filter(Geo.ip == geo.ip).first()
+
+        geo_from_db.ip = geo.ip,
+        geo_from_db.continent = geo.continent,
+        geo_from_db.continent_code = geo.continent_code,
+        geo_from_db.country = geo.country,
+        geo_from_db.country_iso = geo.country_iso,
+        geo_from_db.city = geo.city,
+        geo_from_db.latitude = geo.latitude,
+        geo_from_db.longitude = geo.longitude,
+        geo_from_db.time_zone = geo.time_zone,
+        geo_from_db.postal = geo.postal,
+        geo_from_db.metro_code = geo.metro_code
+
+        self.db.commit()
+        self.db.refresh(geo_from_db)
+
+        return geo_from_db
+
+    def update_with_country(self, geo: GeoSchemaFromCountryJson) -> Geo:
+        geo_from_db: Geo = self.db.query(Geo).filter(Geo.ip == geo.ip).first()
+
+        geo_from_db.ip = geo.ip,
+        geo_from_db.continent = geo.continent,
+        geo_from_db.continent_code = geo.continent_code,
+        geo_from_db.country = geo.country,
+        geo_from_db.country_iso = geo.country_iso
+
+        self.db.commit()
+        self.db.refresh(geo_from_db)
+
+        return geo_from_db
+
     def ip_entry_exists(self, ip: str) -> bool:
         return self.db.query(Geo).filter(Geo.ip == ip).first() is not None
