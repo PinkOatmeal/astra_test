c4843694b8c46a276194b3e5f84fec6cf60324ae
Ilya Zhalnin <mr.ilay333@gmail.com>
2021-06-30 23:33:05 +0300

    Добавляет обработку исключений

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from src.controllers.geolite_controller import GeoliteController
 from src.db.database import Base, engine, SessionLocal
+from src.exceptions.exceptions import InvalidIPProvided, IPNotFound, ReservedIPProvided
 from src.schemas.Geo import GeoSchemaCountry, GeoSchemaCity, GeoSchemaFull
 
 Base.metadata.create_all(bind=engine)
@@ -21,16 +22,37 @@ def get_db():
 @app.get("/country/{ip}", response_model=GeoSchemaCountry)
 def get_country_info(ip: str, db: Session = Depends(get_db)):
     controller = GeoliteController(db)
-    return controller.fetch_country_info(ip)
+    try:
+        return controller.fetch_country_info(ip)
+    except ReservedIPProvided as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except InvalidIPProvided as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except IPNotFound as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
 
 @app.get("/city/{ip}", response_model=GeoSchemaCity)
 def get_city_info(ip: str, db: Session = Depends(get_db)):
     controller = GeoliteController(db)
-    return controller.fetch_city_info(ip)
+    try:
+        return controller.fetch_city_info(ip)
+    except ReservedIPProvided as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except InvalidIPProvided as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except IPNotFound as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
 
 @app.get("/full/{ip}", response_model=GeoSchemaFull)
 def get_full_info(ip: str, db: Session = Depends(get_db)):
     controller = GeoliteController(db)
-    return controller.fetch_city_info(ip)
+    try:
+        return controller.fetch_city_info(ip)
+    except ReservedIPProvided as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except InvalidIPProvided as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except IPNotFound as e:
+        raise HTTPException(status_code=404, detail=str(e))
